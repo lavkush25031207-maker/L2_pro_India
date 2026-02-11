b45f9cd2410d8b9b0adb7e150145cd538593b8e3
Lavkush Maurya <lavkush25031207@gmail.com>
2026-02-11 17:52:21 +0530

    commit

--- js/nav.js
+++ js/nav.js
@@ -3,10 +3,11 @@ document.getElementById('con-menu').addEventListener('click', function() {
     const contentBar = document.querySelector('.content-bar');
     const introVideo = document.querySelector('.intro-video');
     const content = document.querySelector('.content');
+    const cardSection = document.querySelector('#cardSection');
     
     contentBar.classList.toggle('collapsed');
     introVideo.classList.toggle('collapsed');
-
+    cardSection.classList.toggle('collapsed')
 });
 
 
